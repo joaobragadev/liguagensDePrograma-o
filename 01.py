c4fef6a27e35b5ac96c1b35e6778b3c43c65adb7
joaobragadev <110862405+joaobragadev@users.noreply.github.com>
2025-02-07 20:03:21 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,1 +1,10 @@
-tempo = float(input('Digite o tempo gasto(HH:MM))
+tempo = float(input('Digite o tempo gasto(HH:MM) : ')
+velocidade = int(input('digite a velocidade  media: '))
+distancia = tempo * velocidade
+litros_usados = distancia / 12
+
+
+print(f'o  tempo gasto foi de {tempo:.2f} horas')
+print(f'A velocidade media foi de {velodade} km/h')
+print(f' a distancia percorrida foi de {distancia} km')
+print(f'a quantidade de combustivel foi de {litros_usados:.2f} Litros')
